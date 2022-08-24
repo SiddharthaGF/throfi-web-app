afa637e445e6cce9a8211c7ca0ffa2d255fc9187
SiddharthaGF <pablo.guerra@espoch.edu.ec>
2022-08-24 17:24:58 -0500

    Initial commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link, Head } from '@inertiajs/inertia-react';
+
+export default function Welcome(props) {
+    return (
+        <>
+            <h1 className="text-3xl">
+                Hello world!
+            </h1>
+        </>
+    )}
